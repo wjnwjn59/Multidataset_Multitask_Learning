9a81ac4a9772dfd45ac98279468bd4fa5a0189e1
wjnwjn59 <thangduong.no01@gmail.com>
2023-04-15 14:41:33 +0700

    add training for single task

--- train.py
+++ train.py
@@ -8,20 +8,82 @@ import argparse
 import yaml
 import matplotlib.pyplot as plt
 
-from torchvision import transforms
-from torch.utils.data import DataLoader, ConcatDataset, random_split
-from dataset.dataset import *
-from networks.VGGHybridNet import *
+from torch.utils.data import DataLoader, random_split
+from networks.VGGHybridNet import VGGHybridNet
+from networks.SingleVGGNet import SingleVGGNet
 
-def train(
-        model, 
-        dataset,
-        optimizer, 
-        scheduler,
-        criterions, 
-        num_epochs, 
-        device
-    ):
+from utils.create_dataset import (
+    choose_single_dataset, 
+    choose_multi_dataset
+)
+
+def train_singletask(
+    model, 
+    dataset,
+    optimizer, 
+    #scheduler,
+    criterion, 
+    num_epochs, 
+    device
+):
+    train_dataloader, val_dataloader = dataset
+    for epoch in range(num_epochs):
+        model.train()
+        
+        train_loss = 0.0
+        running_items = 0
+        for idx, (inputs, labels) in enumerate(train_dataloader):
+            images, ids = inputs
+            images = images.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+
+            outputs = model(images)
+
+            loss = criterion(outputs, labels)
+
+            loss.backward()
+
+            optimizer.step()
+            
+            running_items += images.size(0)
+            train_loss += loss.item() * images.size(0)
+            
+        train_loss = train_loss / running_items
+        
+        model.eval()
+
+        val_loss = 0.0
+        running_items = 0
+        with torch.no_grad():
+            for idx, (inputs, labels) in enumerate(val_dataloader):
+                images, ids = inputs
+                images = images.to(device)
+                labels = labels.to(device)
+                
+                outputs = model(images)
+
+                loss = criterion(outputs, labels)
+
+                running_items += images.size(0)
+            
+                val_loss += loss.item() * images.size(0)
+                
+        val_loss = val_loss / running_items
+        
+        print(f'Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}')
+
+
+def train_multitask(
+    model, 
+    dataset,
+    optimizer, 
+    #scheduler,
+    criterions, 
+    num_epochs, 
+    device
+):
     
     train_dataloader = dataset[0]
     val_dataloader = dataset[1]
@@ -67,7 +129,7 @@ def train(
 
             optimizer.step()
 
-            scheduler.step()
+            #scheduler.step()
             
             running_items += images.size(0)
             train_loss += loss.item() * images.size(0)
@@ -118,6 +180,19 @@ def train(
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
+    	'--type', 
+    	type=str,
+        required=True,
+        choices=['single', 'hybrid'],
+    	help='Type of network to train'
+    )
+    parser.add_argument(
+    	'--dataset', 
+    	nargs='+',
+        required=True,
+    	help='Specify the dataset to train'
+    )
+    parser.add_argument(
     	'--epochs', 
     	type=int, 
         default=50,
@@ -141,35 +216,23 @@ def main():
     )
     args = parser.parse_args()
 
-
     with open(args.config_path, 'r') as f:
         CONFIG = yaml.load(f, Loader=yaml.FullLoader)
-
-    image_transforms = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-    ])
 
     dataset_cfg = CONFIG['dataset']
     train_cfg = CONFIG['train']
 
-    task1_dataset = Task1Dataset(
-        dataset_cfg['wonders_of_world']['root_dir'],
-        image_transforms
-    )
-    task2_dataset = Task2Dataset(
-        dataset_cfg['pizza_not_pizza']['root_dir'],
-        image_transforms
-    )
-    concat_dataset = ConcatDataset([task1_dataset, task2_dataset])
+    if args.type == 'single':
+        task_dataset = choose_single_dataset(args.dataset[0], dataset_cfg)
+    elif args.type == 'hybrid':
+        task_dataset = choose_multi_dataset(args.dataset, dataset_cfg)
 
-    train_size = int(len(concat_dataset) * train_cfg['train_size'])
-    val_size = int(len(concat_dataset) * train_cfg['val_size'])
-    test_size = len(concat_dataset) - train_size - val_size
+    train_size = int(len(task_dataset) * train_cfg['train_size'])
+    val_size = int(len(task_dataset) * train_cfg['val_size'])
+    test_size = len(task_dataset) - train_size - val_size
 
     train_dataset, val_dataset, test_dataset = random_split(
-        concat_dataset, [train_size, val_size, test_size]
+        task_dataset, [train_size, val_size, test_size]
     )
 
     train_dataloader = DataLoader(
@@ -189,34 +252,60 @@ def main():
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    model = VGGHybridNet(
-        dataset_cfg['wonders_of_world']['n_classes'],
-        dataset_cfg['pizza_not_pizza']['n_classes']
-    ).to(device)
+    if args.type == 'single':
+        model = SingleVGGNet(
+            dataset_cfg[args.dataset[0]]['n_classes'],
+        ).to(device)
 
-    criterion_1 = nn.CrossEntropyLoss()
-    criterion_2 = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(
+            model.parameters(), 
+            lr=args.learning_rate
+        )
+        # scheduler = optim.lr_scheduler.StepLR(
+        #     optimizer, 
+        #     step_size=10, 
+        #     gamma=0.1
+        # )
+        criterion = nn.CrossEntropyLoss()
 
-    optimizer = optim.Adam(
-        model.parameters(), 
-        lr=args.learning_rate,
-        weight_decay=train_cfg['weight_decay']
-    )
-    scheduler = optim.lr_scheduler.StepLR(
-        optimizer, 
-        step_size=10, 
-        gamma=0.1
-    )
+        train_singletask(
+            model=model,
+            dataset=(train_dataloader, val_dataloader),
+            optimizer=optimizer,
+            #scheduler=scheduler,
+            criterion=criterion,
+            num_epochs=args.epochs,
+            device=device
+        )
+    elif args.type == 'hybrid':
+        model = VGGHybridNet(
+            dataset_cfg[args.dataset[0]]['n_classes'],
+            dataset_cfg[args.dataset[1]]['n_classes']
+        ).to(device)
 
-    train(
-        model=model,
-        dataset=(train_dataloader, val_dataloader),
-        optimizer=optimizer,
-        scheduler=scheduler,
-        criterions=(criterion_1, criterion_2),
-        num_epochs=args.epochs,
-        device=device
-    )
+        optimizer = optim.Adam(
+            model.parameters(), 
+            lr=args.learning_rate,
+            weight_decay=train_cfg['weight_decay']
+        )
+        # scheduler = optim.lr_scheduler.StepLR(
+        #     optimizer, 
+        #     step_size=10, 
+        #     gamma=0.1
+        # )
+
+        criterion_1 = nn.CrossEntropyLoss()
+        criterion_2 = nn.CrossEntropyLoss()
+
+        train_multitask(
+            model=model,
+            dataset=(train_dataloader, val_dataloader),
+            optimizer=optimizer,
+            #scheduler=scheduler,
+            criterions=(criterion_1, criterion_2),
+            num_epochs=args.epochs,
+            device=device
+        )
     
 if __name__ == '__main__':
     main()
